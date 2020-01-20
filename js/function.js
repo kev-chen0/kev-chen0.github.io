901f25df100ce36a281700114a5b4252c9a5022c
kevin-chen <kc3585@nyu.edu>
2020-01-20 15:38:48 -0500

    stop preloader

--- js/function.js
+++ js/function.js
@@ -41,14 +41,14 @@ $(document).ready(function() {
     }
   })
 
-  // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-  });
+  // // Preloader
+  // $(window).on('load', function () {
+  //   if ($('#preloader').length) {
+  //     $('#preloader').delay(100).fadeOut('slow', function () {
+  //       $(this).remove();
+  //     });
+  //   }
+  // });
 
   // Back to top button
   $(window).scroll(function() {
